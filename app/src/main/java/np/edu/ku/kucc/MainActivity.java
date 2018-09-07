b1fbf96741ca461fd225affb53ec9a693c7ff9f1
ayushkumarshah <ayush.kumar.shah@gmail.com>
2018-09-07 23:21:32 +0545

    The basic design (Navigation drawer menus) and created fragments

--- app/src/main/java/np/edu/ku/kucc/MainActivity.java
+++ app/src/main/java/np/edu/ku/kucc/MainActivity.java
@@ -1,9 +1,9 @@
 package np.edu.ku.kucc;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import np.edu.ku.kucc.Account.Profile;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,14 +25,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +34,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        changeFragment(new NewsFragment());
     }
 
     @Override
@@ -80,22 +76,48 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+       if (id == R.id.nav_calendar) {
+           changeFragment(new Calendar());
+        } else if (id == R.id.nav_news) {
+           changeFragment(new NewsFragment());
+        } else if (id == R.id.nav_events) {
+           changeFragment(new Events());
+        } else if (id == R.id.nav_routines) {
+           changeFragment(new Routines());
+        } else if (id == R.id.nav_courseinfo) {
+           changeFragment(new Course_info());
+        } else if (id == R.id.nav_teacherinfo) {
+           changeFragment(new Teacher_info());
         }
+        else if (id == R.id.nav_aboutkucc) {
+           changeFragment(new AboutKUCC());
+        }
+         else if (id == R.id.nav_share) {
+
+            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, "Check out KUCC App at: https://play.google.com/store/apps/details?id=" + getPackageName());
+            startActivity(Intent.createChooser(sharingIntent, "Share using"));
+        }
+       else if (id == R.id.nav_profile) {
+           changeFragment(new Profile());
+       } else if (id == R.id.nav_visit) {
+
+       }
+       else if (id == R.id.nav_reviews) {
+           changeFragment(new Reviews());
+       }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void changeFragment(Fragment targetFragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.activity_main, targetFragment, "fragment")
+                .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .commit();
     }
 }
---
+++ app/src/main/java/np/edu/ku/kucc/Teacher_info.java
@@ -0,0 +1,29 @@
+package np.edu.ku.kucc;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Teacher_info extends Fragment {
+
+
+    public Teacher_info() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_teacher_info, container, false);
+    }
+
+}
